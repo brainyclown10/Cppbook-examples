8dfbbe7e38e11310559dfb843b5534fe4ac84b54
Brandon Hu <brandon2009731@gmail.com>
2017-12-28 17:48:44 -0500

    Initial Commit

---
+++ First Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  First Project
+//
+//  Created by Brandon Hu on 12/28/17.
+//  Copyright © 2017 Brandon Hu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
