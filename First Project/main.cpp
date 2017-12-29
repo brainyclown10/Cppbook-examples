fc4bddc2f11a54b516f05ab4d632068f6889f597
Brandon Hu <brandon2009731@gmail.com>
2017-12-28 20:26:12 -0500

    Changed program to calculate the sum from 50 to 100 inclusive.

--- First Project/main.cpp
+++ First Project/main.cpp
@@ -10,13 +10,13 @@
 
 int main()
 {
-    int sum = 0, val = 1;
+    int sum = 0, val = 50;
     // keep executing the while as long as val is less than or equal to 10
-    while (val <= 10) {
+    while (val <= 100) {
         sum += val;    // assigns sum + val to sum
         ++val;    // add 1 to val
     }
-    std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+    std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
     return 0;
 }
 
