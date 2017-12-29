dd5d009e7df69a3f5890ab23435d8b4dd19e761f
Brandon Hu <brandon2009731@gmail.com>
2017-12-28 19:12:33 -0500

    Simple two number sum app.

--- First Project/main.cpp
+++ First Project/main.cpp
@@ -3,13 +3,16 @@
 //  First Project
 //
 //  Created by Brandon Hu on 12/28/17.
-//  Copyright © 2017 Brandon Hu. All rights reserved.
-//
+//  Copyright © 2017 Brandon Hu. All rights x
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
     return 0;
 }
+
