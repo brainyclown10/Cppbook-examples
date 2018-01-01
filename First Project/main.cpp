87731e2a942bbf19e5b0657dbbc0a9b26e35c3a8
Brandon Hu <brandon2009731@gmail.com>
2018-01-01 16:06:30 -0500

    Change main.cpp into while loop example.

--- First Project/main.cpp
+++ First Project/main.cpp
@@ -10,11 +10,9 @@
 
 int main()
 {
-    int sum = 0;
-    // sum values from 1 to 10 inclusive
-    for  (int val = 1; val <= 10; ++val)
-        sum += val; // equivalent to sum = sum + val
-    std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+    int sum = 0, value = 0;
+    while (std::cin >> value)
+        sum += value;    // equivalent to sum = sum + value
+    std::cout << "Sum is " << sum << std::endl;
     return 0;
 }
-
