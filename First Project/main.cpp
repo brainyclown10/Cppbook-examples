01ff66c9234e56ece00c9d606d8c14ac7c0ea608
Brandon Hu <brandon2009731@gmail.com>
2018-01-01 16:14:06 -0500

    Changed main to program that counts the occurrences of a specific number give an unknown number of inputs.

--- First Project/main.cpp
+++ First Project/main.cpp
@@ -10,9 +10,21 @@
 
 int main()
 {
-    int sum = 0, value = 0;
-    while (std::cin >> value)
-        sum += value;    // equivalent to sum = sum + value
-    std::cout << "Sum is " << sum << std::endl;
+    // currVal is the number we're counting; we'll read new values into val
+    int currVal = 0, val = 0;
+    if (std::cin >> currVal) {
+        int cnt = 1; // store the count for the current value we're processing
+        while (std::cin >> val) { // read the reamining numbers
+            if (val == currVal) // if the values are the same
+                ++cnt; // add 1 to cnt
+            else { // otherwise, print the count for the previous value
+                std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+                currVal = val; // remember the new value
+                cnt = 1; // reset the counter
+            }
+        } // while loop ends here
+        // remember to prin the count for the last value in the file
+        std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+    } // outermost if statement ends here
     return 0;
 }
